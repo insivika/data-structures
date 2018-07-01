e309183b9c98ec3f42d76548514cbfe796fc3042
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-07-01 12:20:40 -0700

    Set up shared stack methods and created push

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  
+
+
+  storage = {}
+
+  _.extend(Stack, Stack.stackMethods)
+
+  return 
 };
 
-var stackMethods = {};
+Stack.stackMethods = {}
 
+Stack.stackMethods.push = function(){
 
+};
+
+Stack.stackMethods.pop = function(){
+
+};
+
+Stack.stackMethods.size = function(){
+
+};
+
+console.log(Stack.stackMethods)
