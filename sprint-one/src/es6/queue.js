4b072e5c735c0e928c338676a361708fe02dd3b9
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-07-02 22:10:32 -0700

    Completed queue  for ES6 class declaration

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,51 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  
   constructor() {
+  
+    this.counter = 0;
+
+    this.indexArr = [];
+
+    this.storage = {};
+
+  }
+
+  enqueue(value){
+
+    this.storage[this.counter] = value;
+
+    this.indexArr.push(this.counter);
+
+    this.result = this.storage[this.counter];
+
+    this.counter++;
+
+    return this.result;
+
+  }
+
+  dequeue(){
+
+    this.result = this.storage[this.indexArr[0]];
+
+    delete this.storage[this.indexArr.shift()];
+
+    return this.result;
+
+  }
+
+  size(){
+
+    if(this.indexArr.length < 0){
+
+      return 0;
+
+    } else {
+
+      return this.indexArr.length;
+
+    }
+
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,49 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // 
   constructor() {
+
+    this.storage = {};
+
+    this. counter = 0;
+
+  }
+
+  push(value){
+
+    this.storage[this.counter] = value;
+
+    this.result = this.storage[this.counter];
+
+    this.counter++;
+
+    return this.result;
+
+  }
+
+  pop(){
+
+    this.counter--;
+
+    this.result = this.storage[this.counter];
+
+    delete this.storage[this.counter];
+
+    return this.result;
+
+  }
+
+  size(){
+
+    if(this.counter < 0){
+    
+      return 0;
+
+    } else {
+
+      return this.counter;
+
+    }
+
   }
 
 }
